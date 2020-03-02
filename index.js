a5e4f0cae677d0a1faaeadddd9ac1d5784e090e0
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-02 14:59:50 +1300

    monday one

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("Hello World");
+
+var firstName = "Daniel"; //Assigning a variable called "firstName" to Daniel
+console.log(firstName); //Prints "Daniel" to console
+
+var lastName = "Ewers";
+console.log(lastName);
+
+console.log(firstName + lastName); //Putting two together by adding + inbetween
+
+var firstNumber = 15;
+
+var secondNumber = 25;
+
+console.log("secondNumber(25) + firstNumber(15)");// prints the equation to the console before the answer
+
+
+var sum = secondNumber + firstNumber; //Sum is equal to the sum of the two previous intergers "secondNumber + firstNumber"
+
+console.log(sum);
+
+console.log("secondNumber(25) - firstNumber(15)");
+
+var minus = secondNumber - firstNumber; //subtracts secondNumber from firstnumber and stores it as a variable called "minus"
+
+console.log(minus);
+
+var devide = secondNumber / firstNumber;
+
+console.log("secondNumber(25) / firstNumber(15)");
+
+console.log(devide);
+
+
+
+
